598497228af2c8766a83fd06d6f88509de97e29b
Roman <roomen817@gmail.com>
2025-01-13 17:25:58 +0100

    + infrastructure

--- src/main/java/com/nce/backend/cars/infrastructure/jpa/CarJpaEntity.java
+++ src/main/java/com/nce/backend/cars/infrastructure/jpa/CarJpaEntity.java
@@ -1,12 +1,17 @@
 package com.nce.backend.cars.infrastructure.jpa;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Entity(name = "car")
 @Table(name = "car")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class CarJpaEntity {
 
     @Id
--- src/main/java/com/nce/backend/cars/infrastructure/jpa/CarJpaEntityMapper.java
+++ src/main/java/com/nce/backend/cars/infrastructure/jpa/CarJpaEntityMapper.java
@@ -1,4 +1,7 @@
 package com.nce.backend.cars.infrastructure.jpa;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarJpaEntityMapper {
 }
