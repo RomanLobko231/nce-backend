5081b5053aeb30f33745f01bc74dcc47f277b150
Roman <roomen817@gmail.com>
2025-02-06 18:14:47 +0100

    + error fallback

--- src/main/java/com/nce/backend/cars/infrastructure/externalApi/VegvesenApiService.java
+++ src/main/java/com/nce/backend/cars/infrastructure/externalApi/VegvesenApiService.java
@@ -3,12 +3,16 @@ package com.nce.backend.cars.infrastructure.externalApi;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.nce.backend.cars.domain.services.ExternalApiService;
 import com.nce.backend.cars.domain.valueObjects.ApiCarData;
+import com.nce.backend.cars.domain.valueObjects.GearboxType;
+import com.nce.backend.cars.domain.valueObjects.OperatingMode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
 
 //'Vegvesen' is the name for the Norwegian road inspection agency.
 @Service
@@ -37,6 +41,22 @@ public class VegvesenApiService implements ExternalApiService {
 
         return apiResponse
                 .map(jsonMapper::mapFromJson)
+                .onErrorReturn(
+                        ApiCarData.builder()
+                                .firstTimeRegisteredInNorway(LocalDate.parse("1111-11-11"))
+                                .nextEUControl(LocalDate.parse("1111-11-11"))
+                                .model("N/A")
+                                .make("N/A")
+                                .color("N/A")
+                                .bodywork("N/A")
+                                .engineType("N/A")
+                                .weight(0)
+                                .engineVolume(0)
+                                .numberOfDoors(0)
+                                .numberOfSeats(0)
+                                .operatingMode(OperatingMode.OTHER)
+                                .gearboxType(GearboxType.OTHER)
+                                .build())
                 .block();
     }
 }
