cec2260336063f43427509364707e8fd560e5aa0
Roman <roomen817@gmail.com>
2025-02-12 21:07:58 +0100

    + multiple files upload/delete

--- src/main/java/com/nce/backend/cars/domain/services/ImageStorageService.java
+++ src/main/java/com/nce/backend/cars/domain/services/ImageStorageService.java
@@ -2,9 +2,11 @@ package com.nce.backend.cars.domain.services;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
-public interface ImageStorageService {
-    String uploadImage(String fileName, InputStream inputStream, String contentType) throws IOException;
-    InputStream downloadImage(String fileName);
+public interface ImageStorageService<T> {
+    String uploadImage(T image);
+    List<String> uploadImages(List<T> images);
     void deleteImage(String fileName);
+    void deleteImages(List<String> fileNames);
 }
